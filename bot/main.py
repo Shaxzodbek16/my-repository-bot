f729583d3a85b52c7f80ef7d32f6580015213247
Shaxzodbek <134439368+Shaxzodbek16@users.noreply.github.com>
2024-06-10 23:32:45 +0500

    config

--- bot/main.py
+++ bot/main.py
@@ -1,1 +1,14 @@
-from telebot import TeleBot
+import os
+from telebot import TeleBot, types
+
+bot_token = os.environ.get('BOT_API')
+bot = TeleBot(token=bot_token)
+
+
+@bot.message_handler(commands=['start'])
+def start(message: types.Message) -> None:
+	bot.send_message(message.from_user.id, "Assalomu alaykum, Shaxzodbekning botiga xush kelibsiz!")
+
+
+bot.polling()
+
