5cf42ee91b2d122c24276b43adb087c56ef27f69
Shaxzodbek <134439368+Shaxzodbek16@users.noreply.github.com>
2024-06-12 12:33:45 +0500

    add woking buttons

--- bot/main.py
+++ bot/main.py
@@ -1,13 +1,56 @@
 from config import BOT_API
 from telebot import TeleBot, types
+import buttons
+import information
 
 bot = TeleBot(token=BOT_API)
 
 
+# @bot.message_handler(commands=['start'])
+# def start(message: types.Message) -> None:
+# 	text: str = "Telefon raqamingizni kiriting"
+# 	bot.send_message(message.chat.id, text=text, reply_markup=buttons.information_container)
+#
+#
+# @bot.message_handler(content_types=['contact'])
+# def contact(message: types.Message) -> None:
+# 	text: str = f"Salom, {message.contact.first_name}, {message.contact.last_name}"
+# 	bot.send_message(message.from_user.id, text=text)
+
+
 @bot.message_handler(commands=['start'])
-def start(message: types.Message) -> None:
-	bot.send_message(message.from_user.id, "Assalomu alaykum, Shaxzodbekning botiga xush kelibsiz!")
+def buttons_show(message: types.Message) -> None:
+	text: str = ("Quyidagilardan birini tanlang 👇")
+	bot.send_message(message.from_user.id, text=text, reply_markup=buttons.main_keyboard)
+
+
+@bot.message_handler()
+def main_keyboard(message: types.Message) -> None:
+	match message.text:
+		case "Shaxzodbek👤":
+			bot.send_message(message.from_user.id, text=information.shaxzodbek)
+		case "People":
+			bot.send_message(message.from_user.id, text=information.people)
+		case "Wish✨":
+			bot.send_message(message.from_user.id, text=information.wish)
+		case "Study👨‍🎓":
+			bot.send_message(message.from_user.id, text=information.study)
+		case "Family":
+			bot.send_message(message.from_user.id, text=information.family)
+		case "Friend":
+			bot.send_message(message.from_user.id, text=information.friend)
+		case "Game":
+			bot.send_message(message.from_user.id, text=information.game)
+		case "Love💕":
+			bot.send_message(message.from_user.id, text=information.love)
+		case "Copilot":
+			bot.send_message(message.from_user.id, text=information.Copilot)
+		case "Gemini":
+			bot.send_message(message.from_user.id, text=information.Gemini)
+		case "ChatGPT":
+			bot.send_message(message.from_user.id, text=information.ChatGPT)
+		case _:
+			bot.send_message(message.from_user.id, text=information.default)
 
 
 bot.polling()
-
