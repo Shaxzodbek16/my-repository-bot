73585cbd4120ce8e93c20bf34ef889ce3a01e652
Shaxzodbek <134439368+Shaxzodbek16@users.noreply.github.com>
2024-06-17 01:13:55 +0500

    faqat kerakli buttonlar qoldirildi

--- bot/buttons.py
+++ bot/buttons.py
@@ -1,11 +1,5 @@
 from telebot import types
 
-#  take info from user
-information_container = types.ReplyKeyboardMarkup(resize_keyboard=True)
-information_taker = types.KeyboardButton("Share contact", request_contact=True)
-information_container.add(information_taker,)
-
-
 # about database information
 main_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
 shaxzodbek = types.KeyboardButton("Shaxzodbek👤")
@@ -16,8 +10,5 @@ family = types.KeyboardButton("Family")
 friend = types.KeyboardButton("Friend")
 game = types.KeyboardButton("Game")
 love = types.KeyboardButton("Love💕")
-copilot = types.KeyboardButton("Copilot")
-gemini = types.KeyboardButton("Gemini")
-chatGPT = types.KeyboardButton("ChatGPT")
 
-main_keyboard.add(shaxzodbek, people, wish, study, family, friend, game, love, copilot, gemini, chatGPT)
+main_keyboard.add(shaxzodbek, people, wish, study, family, friend, game, love)
