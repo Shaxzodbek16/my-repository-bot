372b3cd3a848aa8a29b96d02f14fb3a75585ca46
Shaxzodbek <134439368+Shaxzodbek16@users.noreply.github.com>
2024-06-17 01:15:48 +0500

    API dan o'qib turib ma'lumot olib kelinyapti

--- bot/main.py
+++ bot/main.py
@@ -6,18 +6,6 @@ import information
 bot = TeleBot(token=BOT_API)
 
 
-# @bot.message_handler(commands=['start'])
-# def start(message: types.Message) -> None:
-# 	text: str = "Telefon raqamingizni kiriting"
-# 	bot.send_message(message.chat.id, text=text, reply_markup=buttons.information_container)
-#
-#
-# @bot.message_handler(content_types=['contact'])
-# def contact(message: types.Message) -> None:
-# 	text: str = f"Salom, {message.contact.first_name}, {message.contact.last_name}"
-# 	bot.send_message(message.from_user.id, text=text)
-
-
 @bot.message_handler(commands=['start'])
 def buttons_show(message: types.Message) -> None:
 	text: str = "Quyidagilardan birini tanlang 👇"
@@ -28,29 +16,21 @@ def buttons_show(message: types.Message) -> None:
 def main_keyboard(message: types.Message) -> None:
 	match message.text:
 		case "Shaxzodbek👤":
-			bot.send_message(message.from_user.id, text=information.shaxzodbek)
+			bot.send_message(message.from_user.id, text=information.shaxzodbek(0))
 		case "People":
-			bot.send_message(message.from_user.id, text=information.people)
+			bot.send_message(message.from_user.id, text=information.people(0)["people"][0]["first_name"])
 		case "Wish✨":
-			bot.send_message(message.from_user.id, text=information.wish)
+			bot.send_message(message.from_user.id, text=information.wish(0)['title'])
 		case "Study👨‍🎓":
-			bot.send_message(message.from_user.id, text=information.study)
+			bot.send_message(message.from_user.id, text=information.shaxzodbek(0, "study"))
 		case "Family":
-			bot.send_message(message.from_user.id, text=information.family)
+			bot.send_message(message.from_user.id, text=information.shaxzodbek(0, "family"))
 		case "Friend":
-			bot.send_message(message.from_user.id, text=information.friend)
+			bot.send_message(message.from_user.id, text=information.shaxzodbek(0, "friend"))
 		case "Game":
-			bot.send_message(message.from_user.id, text=information.game)
+			bot.send_message(message.from_user.id, text=information.shaxzodbek(0, "game"))
 		case "Love💕":
-			bot.send_message(message.from_user.id, text=information.love)
-		case "Copilot":
-			bot.send_message(message.from_user.id, text=information.Copilot)
-		case "Gemini":
-			bot.send_message(message.from_user.id, text=information.Gemini)
-		case "ChatGPT":
-			bot.send_message(message.from_user.id, text=information.ChatGPT)
-		case _:
-			bot.send_message(message.from_user.id, text=information.default)
+			bot.send_message(message.from_user.id, text=information.shaxzodbek(0, "love"))
 
 
 bot.polling()
