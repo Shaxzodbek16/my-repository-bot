24e378065f2bcdcf43d5d1b8e43ab23c2eb16e46
Shaxzodbek <134439368+Shaxzodbek16@users.noreply.github.com>
2024-06-17 01:16:05 +0500

    Umumiy apilarni o'qish uchun funcsiya yozildi

---
+++ bot/readAPI.py
@@ -0,0 +1,18 @@
+import json
+import requests
+
+
+def information(pk: int, next_api: str, category: str = None) -> dict:
+	if category is None:
+		response = requests.get(f"http://127.0.0.1:8000/api/{next_api}")
+		data = json.loads(response.text)
+		try:
+			return data[pk]
+		except IndexError:
+			return {"message": f"{pk} not found in {next_api}"}
+	response = requests.get(f"http://127.0.0.1:8000/api/{next_api}/?category={category}")
+	data = json.loads(response.text)
+	try:
+		return data[pk]
+	except IndexError:
+		return {"message": f"{pk} not found in {next_api}'s category"}
